799384c15e11b9705cf4a06c042e3196530782ab
gracelove <govlmo91@gmail.com>
2019-10-27 17:15:54 +0900

    init

---
+++ src/test/java/me/grace/mycontact/MycontactApplicationTests.java
@@ -0,0 +1,13 @@
+package me.grace.mycontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
